f17975a4803ac175bc5a3b96214560ecff5045e5
Mike_Zhang <Mike_Zhang@live.com>
2017-05-01 11:39:14 +0800

    add sync script

---
+++ doPull.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python 
+#-*- coding:utf-8 -*-
+
+import os
+
+def doGitPull():
+    bNameAll = os.popen("git branch -l").read()
+    #print bNameAll
+    bName = ""
+    for item in bNameAll.split('\n'):            
+        if item[0] == '*' :
+            bName = item
+            break
+    #print bName
+    bName = bName.split()[-1]
+    print bName   
+    strCmd = "git pull origin %s:%s" % (bName,bName) 
+    print strCmd
+    os.system(strCmd)
+    
+doGitPull()
+
+
